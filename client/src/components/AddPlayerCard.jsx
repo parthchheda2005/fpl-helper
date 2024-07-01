7c2f7edf08cdda4a6a046fb673cc1da91550a6d0
parthchheda2005 <path.h.chheda@gmail.com>
2024-07-01 20:26:49 +0800

    made the add player card

---
+++ client/src/components/AddPlayerCard.jsx
@@ -0,0 +1,9 @@
+function AddPlayerCard() {
+  return (
+    <button className="w-96 rounded-2xl bg-neutral-800 mx-10 my-8 flex flex-col border-8 border-neutral-500 text-neutral-500 border-dashed hover:border-neutral-300 hover:text-neutral-300 transition items-center justify-center duration-300 text-7xl">
+      Add new player!
+    </button>
+  );
+}
+
+export default AddPlayerCard;
--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
 import CardComponent from "./CardComponent";
+import AddPlayerCard from "./AddPlayerCard";
 
 function MainPage() {
   const [players, setPlayers] = useState([]);
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
+
   useEffect(() => {
     fetch("http://127.0.0.1:8000/players/v1/euros/get")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        // console.log(data.data.euroPlayers);
         setPlayers(data.data.euroPlayers);
       })
       .catch((err) => console.log("failed to fetch"));
   }, [players]);
 
-  const palmer = players.find((el) => el.name === "Cole Palmer");
-
+  const kane = players.find((el) => el.name === "Harry Kane");
   const ronaldo = players.find((el) => el.name === "Cristiano Ronaldo");
-
   const musiala = players.find((el) => el.name === "Jamal Musiala");
 
   return players.length > 0 ? (
     <div className="h-screen min-w-max pt-[7vh] bg-neutral-800 text-stone-100 overflow-y-hidden overflow-x-auto flex">
-      <CardComponent player={palmer} />
+      <CardComponent player={kane} />
       <CardComponent player={ronaldo} />
       <CardComponent player={musiala} />
+      <AddPlayerCard />
     </div>
   ) : (
     <div className="w-32 aspect-square rounded-full relative flex justify-center items-center animate-spin z-40 bg-conic-gradient(white_0deg, white_300deg, transparent_270deg, transparent_360deg) before-animate-spin_2s_linear_infinite before:absolute before:w-[60%] before:aspect-square before:rounded-full before:z-[80] before:bg-conic-gradient(white_0deg, white_270deg, transparent_180deg, transparent_360deg) after:absolute after:w-3/4 after:aspect-square after:rounded-full after:z-[60] after-animate-spin_3s_linear_infinite after:bg-conic-gradient(#065f46_0deg, #065f46_180deg, transparent_180deg, transparent_360deg)">
