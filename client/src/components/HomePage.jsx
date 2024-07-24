9b867e5af8e48526a7760d3d0ea9a0bc9ac3f555
parthchheda2005 <path.h.chheda@gmail.com>
2024-07-24 15:51:00 +0800

    added a fade in animation

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,26 +5,26 @@ function HomePage() {
 
   return (
     <div className="h-screen min-w-max pt-[7vh] bg-neutral-800 text-stone-100 flex justify-center items-center flex-col">
-      <div className="text-5xl text-center font-semibold">
+      <div className="text-5xl text-center font-semibold onLoadHead">
         <h1 className="m-3">Welcome to your</h1>
         <h1 className="m-3">Fantasy Football Helper</h1>
       </div>
       <div className="m-5 text-neutral-800">
         <button
           onClick={() => navigate("/euro-24")}
-          className="bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[150px] text-lg hover:bg-blue-600 hover:text-stone-100 transition duration-300 ease-out"
+          className="onLoadB1 bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[150px] text-lg hover:bg-blue-600 hover:text-stone-100 transition duration-300 ease-out"
         >
           EURO24 Data
         </button>
         <button
           onClick={() => navigate("/fpl")}
-          className="bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[150px] text-lg hover:bg-lime-600 hover:text-stone-100 transition duration-300 ease-out"
+          className="onLoadB1 bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[150px] text-lg hover:bg-lime-600 hover:text-stone-100 transition duration-300 ease-out"
         >
           FPL Data
         </button>
         <button
           onClick={() => navigate("/fpl-squad")}
-          className="bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[200px] text-lg hover:bg-amber-700 hover:text-stone-100 transition duration-300 ease-out"
+          className="onLoadB1 bg-stone-100 rounded-xl mx-6 px-4 py-3 w-[200px] text-lg hover:bg-amber-700 hover:text-stone-100 transition duration-300 ease-out"
         >
           FPL Squad Planning
         </button>
