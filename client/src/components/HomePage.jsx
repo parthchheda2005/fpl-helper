080f16fcfeb3d13d88a9b229d5b375d32e9e0ae9
parthchheda2005 <path.h.chheda@gmail.com>
2024-08-02 17:57:07 +0800

    added more animations on load!

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,33 +1,39 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 function HomePage() {
   const navigate = useNavigate();
 
   return (
     <div className="h-screen w-full sm:pt-[7vh] pt-1 bg-neutral-800 text-stone-100 flex justify-center items-center flex-col">
-      <div className="onLoad">
-        <div className="sm:text-5xl text-center font-semibold w-full text-2xl px-4">
+      <div className="relative">
+        <div
+          className={`relative bounce-in sm:text-5xl text-center font-semibold w-full text-2xl px-4 mb-7`}
+        >
           <h1 className="m-3">Welcome to your</h1>
-          <h1 className="m-3">Fantasy Football Helper</h1>
+          <h1 className="m-3">⚽️ Fantasy Football Helper ⚽️</h1>
         </div>
-        <div className="m-5 text-neutral-800 flex flex-wrap justify-center">
+        <div
+          className={`relative come-in m-5 text-neutral-800 flex flex-wrap justify-center`}
+        >
           <button
             onClick={() => navigate("/euro-24")}
             className="bg-stone-100 rounded-xl mx-2 px-4 py-3 w-[150px] text-lg hover:bg-blue-600 hover:text-stone-100 transition duration-300 ease-out my-2"
           >
-            EURO24 Data
+            🇪🇺 EURO24 Data
           </button>
           <button
             onClick={() => navigate("/fpl")}
             className="bg-stone-100 rounded-xl mx-2 px-4 py-3 w-[150px] text-lg hover:bg-lime-600 hover:text-stone-100 transition duration-300 ease-out my-2"
           >
-            FPL Data
+            🏴󠁧󠁢󠁥󠁮󠁧󠁿 FPL Data
           </button>
           <button
             onClick={() => navigate("/fpl-squad")}
             className="bg-stone-100 rounded-xl mx-2 px-4 py-3 w-[200px] text-lg hover:bg-amber-700 hover:text-stone-100 transition duration-300 ease-out my-2"
           >
-            FPL Squad Planning
+            📊 FPL Squad Planning
           </button>
         </div>
       </div>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -51,7 +51,7 @@ function NavBar() {
   );
 
   return (
-    <nav className="bg-sky-800 text-white fixed w-full">
+    <nav className="bg-sky-800 text-white fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div
